c92ff826f194ed1ef2b420cf4361a110ff955be2
Sharon Clark <clarksl@gmail.com>
2019-07-29 10:16:32 -0600

    include icon module

--- modules/menulist/menulist.module.ts
+++ modules/menulist/menulist.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { MenulistComponent } from './menulist.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { SciedFooterModule } from '../scied-footer/scied-footer.module';
-
+import {MatIconModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
     MatGridListModule,
-    SciedFooterModule
+    SciedFooterModule,
+    MatIconModule
   ],
   declarations: [MenulistComponent],
   exports: [MenulistComponent]
